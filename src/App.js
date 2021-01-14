c44cd85d4347b6079050bf6c254d101ae7c13ca0
Akashamba <akashambashankar@gmail.com>
2021-01-14 14:15:36 +0530

    added appbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Photo from './pages/photo/photo.component.jsx';
 import Homepage from './pages/homepage/homepage.component.jsx';
+import {AppBar, Toolbar} from '@material-ui/core'
 import {Switch, Route, Redirect} from 'react-router-dom';
+import './App.css';
 
 function App() {
     
     return (
         <div className="App">
+            <AppBar position="fixed"><Toolbar><p className="appbar-text">About Me</p></Toolbar></AppBar>
             <Switch>
                 <Route exact path='/' component={Homepage}/>
                 <Route exact path='/profile-picture' component={Photo} />
