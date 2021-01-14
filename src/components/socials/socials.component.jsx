22ecdc8838bac991e81ab50471a3031f6f430fd3
Akashamba <akashambashankar@gmail.com>
2021-01-14 16:58:28 +0530

    added socials

--- src/components/socials/socials.component.jsx
+++ src/components/socials/socials.component.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import {Grid} from '@material-ui/core';
+import './socials.styles.css';
 
 export default function Socials() {
     const icons = [
-        "fab fa-facebook-f",
-        "fab fa-instagram",
-        "fab fa-twitter",
-        "fab fa-github",
-        "fab fa-linkedin-in",
-        "fab fa-medium-m",
-        "fas fa-envelope",
+        {name: "fab fa-instagram", link: "https://www.instagram.com/_akashamba_/"},
+        {name: "fab fa-twitter", link: "https://twitter.com/akashamba238"},
+        {name: "fab fa-github", link: "https://github.com/Akashamba"},
+        {name: "fab fa-linkedin-in", link: "https://www.linkedin.com/in/akash-ambashankar/"},
+        {name: "fab fa-medium-m", link: "https://akashambashankar.medium.com/"},
+        {name: "fas fa-envelope", link: "mailto:akashambashankar@gmail.com"}
     ]
     return(
-        <div id="socials">
-            {icons.map(iconName => (
-                <span className="icon-border" key={iconName}>
-                    <i className={iconName}></i>
-                </span>
+        <Grid container id="socials">
+            {icons.map(icon => (
+                <Grid item xs={2} key={icon}>
+                    <a href={icon.link}><i className={`social-icon ${icon.name}`}></i></a>
+                </Grid>
             ))}
-        </div>
+        </Grid>
     )
 }
