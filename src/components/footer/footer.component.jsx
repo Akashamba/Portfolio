d7404dd246b5008ef0e77eb07b276fac43bc52c3
Akashamba <akashambashankar@gmail.com>
2021-01-15 14:56:44 +0530

    updated footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,6 +4,10 @@ import './footer.styles.css'
 export default function Footer() {
     const resume = "https://drive.google.com/file/d/1J5WNIwbQi8iZHWczaliOyNtQQcNjoK0l/view?usp=sharing";
     return(
-        <div id="footer">Find my resume <a className="project-link" href={resume}>here</a>.</div>
+        <div id="footer">
+            <p>Find my resume <a className="project-link" href={resume}>here</a>.</p>
+            <br/>
+            <p> &copy; Akash Ambashankar</p>
+        </div>
     )
 }
