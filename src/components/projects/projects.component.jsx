396d64367e646510213e1c72edf06256b2cd7db4
Akashamba <akashambashankar@gmail.com>
2021-01-24 12:32:52 +0530

    changes

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -3,8 +3,6 @@ import {projects} from '../../utilities/lists.js';
 import {Grid, Container} from '@material-ui/core';
 import nokia from '../../assets/images/nokia.png';
 import tracker from '../../assets/images/logo.png';
-import telegram from '../../assets/images/telegram.webp';
-import github from '../../assets/images/github.webp';
 import Aos from 'aos';
 import "aos/dist/aos.css";
 import './projects.styles.css';
