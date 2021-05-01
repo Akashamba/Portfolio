9cb7bdf19d9870ff89379ac758dc8b96b57b54b7
Akash Ambashankar <akashambashankar@gmail.com>
2021-05-01 21:30:40 +0530

    deleted app.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Homepage from './pages/homepage/homepage.component.jsx';
 import Navbar from './components/navbar/navbar.component.jsx';
 import Preloader from './components/preloader/preloader.component.jsx';
 import {Switch, Route, Redirect} from 'react-router-dom';
-import './App.css';
 import './animations/load-in.css';
 
 function App() {
