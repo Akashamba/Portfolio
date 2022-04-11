823dd1c872bdb9efbe0698f435d2e2b58289bf9a
Akash Ambashankar <akashambashankar@gmail.com>
2022-04-11 17:08:56 +0530

    updates

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -16,9 +16,30 @@ export default function Projects() {
                 <Container>
                     <Grid container>
                         <Grid className="item-container" data-aos="fade-right" item xs={12} md={6}>
+                            <h3 className="item-heading">Single Sign-On Authentication System</h3>
+                            <p className="item-content">Proposed a Single Sign-On Authentication System that uses Government IDs to authenticate users</p>
+                            <p className="item-content">Patent filed under German Patent and Trademark Office</p>
+                            <p className="item-content"><a className="item-link" href={projects[0].link}>Patent Certificate</a></p>
+                        </Grid>
+
+                        <Grid item xs={12} className="mobile-hr" data-aos="fade-right">
+                            <hr/>
+                        </Grid>
+                        
+                        <Grid className="item-container" data-aos="fade-left" item xs={12} md={6}>
+                            <h3 className="item-heading">Dynamic Branch Predictor</h3>
+                            <p className="item-content">Leading a team of three, in developing a Dynamic Branch Predictor using Deep Learning</p>
+                            <p className="item-content">Working under the supervision of Dr.&nbsp;Chakraborty, NTNU, Trondheim, Norway</p>
+                        </Grid>
+
+                        <Grid item xs={12} className="mobile-hr" data-aos="fade-left">
+                            <hr/>
+                        </Grid>
+
+                        <Grid className="item-container" data-aos="fade-right" item xs={12} md={6}>
                             <h3 className="item-heading">Flight Delay Prediction System</h3>
                             <p className="item-content">A Predictive Machine Learning Engine that can <b>classify delayed flights and predict their delay period</b></p>
-                            <p className="item-content"><a className="item-link" href={projects[5].link}>Source Code</a></p>
+                            <p className="item-content"><a className="item-link" href={projects[2].link}>Source Code</a></p>
                         </Grid>
 
                         <Grid item xs={12} className="mobile-hr" data-aos="fade-right">
@@ -28,52 +49,18 @@ export default function Projects() {
                         <Grid className="item-container" data-aos="fade-left" item xs={12} md={6}>
                             <h3 className="item-heading">Coronavirus Tracker</h3>
                             <p className="item-content">A web app that tracks the spread of Covid-19 worldwide and provides useful data visualization</p>
-                            <p className="item-content"><a className="item-link" href={projects[0].link}>{projects[0].name}</a></p>
+                            <p className="item-content"><a className="item-link" href={projects[3].link}>{projects[3].name}</a></p>
                         </Grid>
 
                         <Grid item xs={12} className="mobile-hr" data-aos="fade-left">
                             <hr/>
                         </Grid>
 
-                        <Grid className="item-container" data-aos="fade-right" item xs={12} md={6}>
+                        <Grid className="item-container" data-aos="fade-right   " item xs={12} md={6}>
                             <h3 className="item-heading">Dictionary App</h3>
                             <p className="item-content">A <b>PWA</b> built on <b>Oxford API</b></p>
                             <p className="item-content"><b>Clean, Intuitive UI</b></p>
-                            <p className="item-content"><a className="item-link" href={projects[1].link}>{projects[1].name}</a></p>
-                        </Grid>
-
-                        <Grid item xs={12} className="mobile-hr" data-aos="fade-right">
-                            <hr/>
-                        </Grid>
-
-                        {/* <Grid className="item-container" data-aos="fade-left" item xs={12} md={6}>
-                            <h3 className="item-heading">Todo Web App</h3>
-                            <p className="item-content"><b>Full Stack PWA</b> to manage daily tasks</p>
-                            <p className="item-content">Easy to use and cross-platform</p>
-                            <p className="item-content"><a className="item-link" href={projects[2].link}>{projects[2].name}</a> (In Development)</p>
-                        </Grid> */}
-
-                        <Grid className="item-container" data-aos="fade-left" item xs={12} md={6}>
-                            <h3 className="item-heading">Gympark Fitness Studio</h3>
-                            <p className="item-content"><b>Stylish, Modern and Professional demo website</b></p>
-                            <p className="item-content"><a className="item-link" href={projects[2].link}>{projects[2].name}</a></p>
-                        </Grid>
-
-                        <Grid item xs={12} className="mobile-hr" data-aos="fade-left">
-                            <hr/>
-                        </Grid>
-
-                        <Grid className="item-container" data-aos="fade-right" item xs={12} md={6}>
-                            <h3 className="item-heading">Telegram Chatbots</h3>
-                            <p className="item-content">Chatbots built in Python to perform various functions</p>
-                        </Grid>
-
-                        <Grid item xs={12} className="mobile-hr" data-aos="fade-left">
-                            <hr/>
-                        </Grid>
-
-                        <Grid className="item-container" data-aos="fade-left" item xs={12} md={6}>
-                            <h3 id="visit-github" className="item-heading">Visit My <a className="item-link" href="https://github.com/Akashamba">GitHub</a> for more projects</h3>
+                            <p className="item-content"><a className="item-link" href={projects[4].link}>{projects[4].name}</a></p>
                         </Grid>
                     </Grid>
                 </Container>
