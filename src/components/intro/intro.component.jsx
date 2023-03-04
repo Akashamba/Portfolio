929c3b736f98a9c8e8eac744d58d0b7026cf5922
akashamba <akashambashankar@gmail.com>
2023-03-04 13:08:14 -0500

    update after starting at concordia

--- src/components/intro/intro.component.jsx
+++ src/components/intro/intro.component.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
-import official from '../../assets/images/official.jpg';
+import React from "react";
+import official from "../../assets/images/official.jpg";
 // import getAge from '../../utilities/age-calculator.js'
-import {Grid, Container} from '@material-ui/core'
-import './intro.styles.css'
+import { Grid, Container } from "@material-ui/core";
+import "./intro.styles.css";
 
 export default function Intro() {
-    return (
-        <Container maxWidth="md" id="intro-container">
-            <Grid container id="intro">
-                <Grid item xs={12} md={7} id="profile-picture-container">
-                    <div id="profile-picture-frame">
-                        <img src={official} alt="Akash Ambashankar" id="profile-picture-closed" />
-                    </div>
-                </Grid>
+  return (
+    <Container maxWidth="md" id="intro-container">
+      <Grid container id="intro">
+        <Grid item xs={12} md={7} id="profile-picture-container">
+          <div id="profile-picture-frame">
+            <img
+              src={official}
+              alt="Akash Ambashankar"
+              id="profile-picture-closed"
+            />
+          </div>
+        </Grid>
 
-                <Grid item xs={12} md={5}>
-                    <div className="content intro-content">
-                        <p id="intro-name" className="add-spacing">Akash Ambashankar</p>
-                        {/*<p className="intro-sub-text">{getAge()} y/o<span className="add-spacing"> • </span>Web Development</p>*/}
-                        <p className="intro-sub-text">Web Development<span className="add-spacing"> • </span>Machine Learning</p>
-                        <p className="intro-sub-text" style={{paddingTop: "9px", fontWeight: "700"}}>Software Engineer</p>
-                        <br/>
-                    </div>
-                </Grid>
-            </Grid>
-        </Container>
-    )
+        <Grid item xs={12} md={5}>
+          <div className="content intro-content">
+            <p id="intro-name" className="add-spacing">
+              Akash Ambashankar
+            </p>
+            {/*<p className="intro-sub-text">{getAge()} y/o<span className="add-spacing"> • </span>Web Development</p>*/}
+            {/* <p className="intro-sub-text">
+              Web Development<span className="add-spacing"> • </span>Machine
+              Learning
+            </p>
+            <p
+              className="intro-sub-text"
+              style={{ paddingTop: "9px", fontWeight: "700" }}
+            >
+              Software Engineer
+            </p> */}
+            <p className="intro-sub-text" style={{ fontWeight: "700" }}>
+              Master's Student at Concordia University
+            </p>
+            <p className="intro-sub-text">Montreal, Canada</p>
+
+            <br />
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
--- src/utilities/lists.js
+++ src/utilities/lists.js
@@ -1,38 +1,51 @@
 export const navigation = [
-    {name: "Home", link: "#home"},
-    {name: "Experience", link: "#experience"},
-    {name: "Projects", link: "#projects"},
-    {name: "Interests", link: "#interests"},
-    {name: "Certifications", link: "#certifications"},
-    {name: "Contact Info", link: "#footer"},
-]
+  { name: "Home", link: "#home" },
+  { name: "Experience", link: "#experience" },
+  { name: "Projects", link: "#projects" },
+  { name: "Interests", link: "#interests" },
+  { name: "Certifications", link: "#certifications" },
+  { name: "Contact Info", link: "#footer" },
+];
 
-export const resume = "https://drive.google.com/file/d/1N1aLrPgl2ZYUlmD4_ZBJiNQ3GK1wyLyI/view?usp=sharing";
+export const resume =
+  "https://drive.google.com/file/d/1N1aLrPgl2ZYUlmD4_ZBJiNQ3GK1wyLyI/view?usp=sharing";
 export const blog = "https://akashamba-blog.netlify.app/";
 
 export const socials = [
-    {name: "fas fa-phone-alt", link: "tel:7397705666"},
-    {name: "fab fa-instagram", link: "https://www.instagram.com/_akashamba_/"},
-    {name: "fab fa-twitter", link: "https://twitter.com/akashamba238"},
-    {name: "fas fa-blog", link: blog},
-    {name: "fab fa-github", link: "https://github.com/Akashamba"},
-    {name: "fab fa-linkedin-in", link: "https://www.linkedin.com/in/akash-ambashankar/"},
-    {name: "fab fa-whatsapp", link: "https://wa.me/+917397705666"},
-    {name: "fas fa-envelope", link: "mailto:akashambashankar@gmail.com"}
+  { name: "fas fa-phone-alt", link: "tel:+14387222704" },
+  { name: "fab fa-instagram", link: "https://www.instagram.com/_akashamba_/" },
+  { name: "fab fa-twitter", link: "https://twitter.com/akashamba238" },
+  { name: "fas fa-blog", link: blog },
+  { name: "fab fa-github", link: "https://github.com/Akashamba" },
+  {
+    name: "fab fa-linkedin-in",
+    link: "https://www.linkedin.com/in/akash-ambashankar/",
+  },
+  { name: "fab fa-whatsapp", link: "https://wa.me/+14387222704" },
+  { name: "fas fa-envelope", link: "mailto:akashambashankar@gmail.com" },
 ];
 
 export const projects = [
-    {name: 'German Patent', link: 'https://drive.google.com/file/d/1TFlC6xAzcwiONRcXzoJSf6gm8ATKjbNh/view?usp=sharing'},
-    {},
-    {name: 'Flight Delay  Prediction System', link: 'https://github.com/Akashamba/Flight-Delay-Prediction'},
-    {name: 'Coronavirus Tracker', link: 'https://coronavirus-tracker-live.netlify.app/'},
-    {name: 'Dictionary App', link: 'https://simple-dictionary.netlify.app'},
+  {
+    name: "German Patent",
+    link: "https://drive.google.com/file/d/1TFlC6xAzcwiONRcXzoJSf6gm8ATKjbNh/view?usp=sharing",
+  },
+  {},
+  {
+    name: "Flight Delay  Prediction System",
+    link: "https://github.com/Akashamba/Flight-Delay-Prediction",
+  },
+  {
+    name: "Coronavirus Tracker",
+    link: "https://coronavirus-tracker-live.netlify.app/",
+  },
+  { name: "Dictionary App", link: "https://simple-dictionary.netlify.app" },
 ];
 
 export const footer = {
-    home: "#home",
-    resume: resume,
-    phone: "tel:7397705666",
-    email: "mailto:akashambashankar@gmail.com",
-    feedback: "http://akashamba-feedback.netlify.app/portfolio",
-}
+  home: "#home",
+  resume: resume,
+  phone: "tel:7397705666",
+  email: "mailto:akashambashankar@gmail.com",
+  feedback: "http://akashamba-feedback.netlify.app/portfolio",
+};
