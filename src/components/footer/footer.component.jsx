65bf149da37da2e39a79fd87aaa8e64242e29045
Akash Ambashankar <akashambashankar@gmail.com>
2024-12-28 22:35:43 -0500

    Changes to experiences and projects content (certifications pending). Removed some sections. Footer: changed url (+ prettier formatting changes)

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,39 +1,61 @@
-import React from 'react';
-import logo from '../../assets/images/logo-t.png';
-import {footer, socials} from '../../utilities/lists.js';
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import './footer.styles.css'
+import React from "react";
+import logo from "../../assets/images/logo-t.png";
+import { footer, socials } from "../../utilities/lists.js";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import "./footer.styles.css";
 
 export default function Footer() {
-    const url = "akashamba.netlify.app";
-    return(
-        <div id="footer">
-            <p>
-                <AnchorLink href={footer.home} className="footer-link">Home</AnchorLink> 
-                &nbsp; | &nbsp; 
-                <a href={footer.resume} className="footer-link">Resumé</a> 
-                &nbsp; | &nbsp;
-                <a href={footer.phone} className="footer-link">Phone</a> 
-                &nbsp; | &nbsp; 
-                <a href={footer.email} className="footer-link">E-mail</a>                 
-                &nbsp; | &nbsp; 
-                <a href={footer.feedback} className="footer-link">Feedback</a>                                 
-            </p>
-            <br/>
-            <a href="/" className="footer-link">{url}</a>
-            <br/><br/>
-            <img src={logo} alt="Akash Ambashankar Logo" className="footer-logo" />
-            <p className="footer-logo-text">The Pentriangle</p>
-            <p> &copy; <a href="/" className="footer-link">Akash Ambashankar</a></p>
-            
-            <p>
-                {socials.map(icon => (
-                        <a key={icon.name} href={icon.link}><i className={`footer-icon ${icon.name}`}></i></a>
-                ))}
-            </p>
-            <br/><br/>
-            <p className="quote">"a good listener is always</p>
-            <p className="quote">a good learner."</p>
-        </div>
-    )
+  const url = "akashamba.me";
+  return (
+    <div id="footer">
+      <p>
+        <AnchorLink href={footer.home} className="footer-link">
+          Home
+        </AnchorLink>
+        &nbsp; | &nbsp;
+        <a href={footer.resume} className="footer-link">
+          Resumé
+        </a>
+        &nbsp; | &nbsp;
+        <a href={footer.phone} className="footer-link">
+          Phone
+        </a>
+        &nbsp; | &nbsp;
+        <a href={footer.email} className="footer-link">
+          E-mail
+        </a>
+        &nbsp; | &nbsp;
+        <a href={footer.feedback} className="footer-link">
+          Feedback
+        </a>
+      </p>
+      <br />
+      <a href="/" className="footer-link">
+        {url}
+      </a>
+      <br />
+      <br />
+      <img src={logo} alt="Akash Ambashankar Logo" className="footer-logo" />
+      <p className="footer-logo-text">The Pentriangle</p>
+      <p>
+        {" "}
+        &copy;{" "}
+        <a href="/" className="footer-link">
+          Akash Ambashankar
+        </a>
+      </p>
+
+      <p>
+        {socials.map((icon) => (
+          <a key={icon.name} href={icon.link}>
+            <i className={`footer-icon ${icon.name}`}></i>
+          </a>
+        ))}
+      </p>
+      <br />
+      <br />
+      <p className="quote">"a good listener is always</p>
+      <p className="quote">a good learner."</p>
+    </div>
+  );
 }
--- src/utilities/content.js
+++ src/utilities/content.js
@@ -1,10 +1,9 @@
 // const grad1 = "linear-gradient(to bottom, rgb(22, 17, 24) 1%, rgb(22, 22, 172))"
-const grad1 = "linear-gradient(to bottom, #2528fd, #017e97)"
-const grad2 = "linear-gradient(to bottom, #017e97,  #09bc8a)"
-const grad3 = "linear-gradient(to bottom, #09bc8a,#4ab438)"
-const grad4 = "linear-gradient(to bottom, #4ab438, #017e97)"
-const grad5 = "linear-gradient(to bottom, #017e97, rgb(22, 17, 24))"
-
+const grad1 = "linear-gradient(to bottom, #2528fd, #017e97)";
+const grad2 = "linear-gradient(to bottom, #017e97,  #09bc8a)";
+const grad3 = "linear-gradient(to bottom, #09bc8a,#4ab438)";
+const grad4 = "linear-gradient(to bottom, #4ab438, #017e97)";
+const grad5 = "linear-gradient(to bottom, #017e97, rgb(22, 17, 24))";
 
 export const content = {
   sections: {
@@ -12,18 +11,31 @@ export const content = {
       sectionHeader: "Experience",
       items: [
         {
+          mainHeading: "Graduate Research Assistant",
+          subHeading: "Concordia University",
+          subsubHeading: "Sep 2022 - Aug 2024",
+          description:
+            "As a funded graduate researcher in the Algorithms and Complexity Lab, my thesis focused on efficient broadcasting algorithms for networks containing intersecting cliques, including windmill graphs and block graphs. I developed polynomial-time solutions for NP-hard problems, advancing information dissemination in complex graph structures.",
+          link: [
+            "Master's Thesis: Broadcasting in graphs containing intersecting cliques",
+            "https://spectrum.library.concordia.ca/id/eprint/994411/",
+          ],
+        },
+        {
           mainHeading: "Software Engineer Intern",
           subHeading: "Basik Marketing: The Esports Club",
-          subSubHeading: "Feb 2022 - Aug 2022",
-          description: "Achieved a 20% performance boost by developing and integrating a custom JavaScript bracketing system supporting formats like single/double elimination and round robin. Partnered with backend teams for scalability and enhanced code maintainability using the Context API."
+          subsubHeading: "Feb 2022 - Aug 2022",
+          description:
+            "As a Software Engineering Intern at The Esports Club, I built a custom tournament bracketing system using React and Next.js, optimized UI development with Tailwind CSS and Storybook, streamlined collaboration, and crafted user-friendly tools for organizers.",
         },
         {
           mainHeading: "Software Engineer Intern",
           subHeading: "Nokia Solutions and Networks",
-          subSubHeading: "Oct 2020 - Jan 2021",
-          description: "Optimized warehouse operations by 40% through the development of a React.js-based inventory management system. Simplified warehouse operations with real-time data visualizations using Charts.js. Boosted team collaboration by providing feedback on code and coordinating tasks in a GitLab environment."
-        }
-        ],
+          subsubHeading: "Oct 2020 - Jan 2021",
+          description:
+            "During my internship at Nokia, I developed a React-based inventory management app, streamlined warehouse operations by 40%, designed real-time data visualizations with Charts.js, and contributed to maintaining clean, efficient team workflows.",
+        },
+      ],
       options: {
         // backgroundImage: "linear-gradient(to top, #4481eb 0%,hsl(195, 99.20%, 50.60%) 100%)",
         backgroundImage: grad1,
@@ -31,129 +43,61 @@ export const content = {
       },
     },
     projects: {
-      sectionHeader: "Projects",
+      sectionHeader: "Projects and Publications",
       items: [
         {
           mainHeading: "Broadcasting in Stars of Cliques",
-          description: "Developed an O(n · log n) algorithm to optimize broadcast time in 'Star of Cliques' networks. Proved algorithmic optimality and explored applications in polynomial solutions for other graph structures. Published at IWOCA 2024 in Ischia, Italy.",
+          description:
+            "At IWOCA 2024, I presented an O(n⋅logn) algorithm for optimizing broadcast time in Star of Cliques networks. This work proved algorithmic optimality, introduced binary-based techniques for tracking unaddressed vertices, and demonstrated its significance in efficient message dissemination. Published in Ischia, Italy.",
           link: [
             "Published Paper",
-            "https://link.springer.com/chapter/10.1007/978-3-031-63021-7_37"
-          ]
+            "https://link.springer.com/chapter/10.1007/978-3-031-63021-7_37",
+          ],
         },
         {
-          mainHeading: "Single Sign-On Authentication System",
-          description: "Proposed a Single Sign-On Authentication System that uses government IDs to authenticate users, incorporating biometric cross-referencing to enhance security. Patent issued by the German Patent and Trademark Office.",
+          mainHeading: "Plagiarism Detector",
+          description:
+            "For my COMP 6651 project, I built a Python-based plagiarism detection tool that integrates n-gram tokenization, cosine similarity, and Levenshtein Distance to accurately detect text similarities. This hybrid approach enhances both efficiency and accuracy in identifying potential plagiarism.",
           link: [
-            "Patent Certificate",
-            "https://drive.google.com/file/d/1TFlC6xAzcwiONRcXzoJSf6gm8ATKjbNh/view?usp=sharing"
-          ]
+            "GitHub",
+            "https://github.com/Akashamba/COMP6651-Plagiarism-Detector",
+          ],
         },
         {
-          mainHeading: "Exploration of Performance of Dynamic Branch Predictors",
-          description: "Achieved up to 98.2% branch prediction accuracy by implementing and evaluating five popular strategies using a simulation framework developed in SimpleScalar. Published findings at ICICICT 2022.",
+          mainHeading: "Coronavirus Tracker",
+          description:
+            "I built a React-based web app to monitor Covid-19, featuring live updates and location-based data. It leverages the covid19india.org API and uses Charts.js to provide clear, interactive visualizations of the pandemic's spread.",
+          link: ["Web App", "https://coronavirus-tracker-live.netlify.app"],
+        },
+        {
+          mainHeading: "SSO Authentication System",
+          description:
+            "As part of a team, I contributed to the development of a Single Sign-On authentication system using government IDs and biometric cross-referencing for secure user verification. The system is patented with the German Patent and Trademark Office.",
+          link: [
+            "Patent certificate",
+            "https://drive.google.com/file/d/1TFlC6xAzcwiONRcXzoJSf6gm8ATKjbNh/view",
+          ],
+        },
+        {
+          mainHeading:
+            "Exploration of Performance of Dynamic Branch Predictors",
+          description:
+            "During my undergraduate capstone project, I led a team to implement and evaluate five dynamic branch prediction strategies using SimpleScalar, analyzing accuracy, MPKI, and IPC. Mentored by Dr. Shounak Chakraborty from the Norwegian University of Science and Technology. Published at ICICICT 2022.",
           link: [
             "Published Paper",
-            "https://ieeexplore.ieee.org/abstract/document/9917915"
-          ]
-        },
-        {
-          mainHeading: "Plagiarism Detection Tool",
-          description: "Designed a Python-based plagiarism detection tool using n-gram tokenization, cosine similarity, and Levenshtein Distance algorithms to improve detection accuracy.",
-          link: [
-            "GitHub Repository",
-            "https://github.com/Akashamba/COMP6651-Plagiarism-Detector"
-          ]
+            "https://ieeexplore.ieee.org/abstract/document/9917915",
+          ],
         },
         {
           mainHeading: "ElasticERL: Equipment History Report Listing",
-          description: "Implemented ElasticERL, a dynamic ADT in Java that optimizes performance and memory usage by adaptively switching between expandable arrays and AVL trees based on data size.",
-          link: [
-            "GitHub Repository",
-            "https://github.com/Akashamba/COMP6481-ElasticERL"
-          ]
-        }
+          description:
+            "For my COMP 6481 project, I created ElasticERL, a dynamic Java-based data structure that switches between expandable arrays and AVL trees, optimizing memory and performance for managing and sorting equipment history reports efficiently.",
+          link: ["GitHub", "https://github.com/Akashamba/COMP6481-ElasticERL"],
+        },
       ],
       options: {
         backgroundImage: grad2,
         backgroundColor: "rgb(95,167,134)",
-      },
-    },
-    technicalSkills: {
-      sectionHeader: "Technical Skills",
-      items: [
-        {
-          mainHeading: "Software Engineer Intern",
-          subHeading: "Basik Marketing: The Esports Club",
-          subSubHeading: "Feb 2022 - Aug 2022",
-          description: "Achieved a 20% performance boost by developing and integrating a custom JavaScript bracketing system supporting formats like single/double elimination and round robin. Partnered with backend teams for scalability and enhanced code maintainability using the Context API."
-        },
-        {
-          mainHeading: "Software Engineer Intern",
-          subHeading: "Nokia Solutions and Networks",
-          subSubHeading: "Oct 2020 - Jan 2021",
-          description: "Optimized warehouse operations by 40% through the development of a React.js-based inventory management system. Simplified warehouse operations with real-time data visualizations using Charts.js. Boosted team collaboration by providing feedback on code and coordinating tasks in a GitLab environment."
-        }
-        ],
-      options: {
-        backgroundImage: grad5,
-        backgroundColor: "#fff",
-      },
-    },
-    education: {
-      sectionHeader: "Experience",
-      items: [
-        {
-          mainHeading: "Software Engineer Intern",
-          subHeading: "Basik Marketing: The Esports Club",
-          subSubHeading: "Feb 2022 - Aug 2022",
-          description: "Achieved a 20% performance boost by developing and integrating a custom JavaScript bracketing system supporting formats like single/double elimination and round robin. Partnered with backend teams for scalability and enhanced code maintainability using the Context API."
-        },
-        {
-          mainHeading: "Software Engineer Intern",
-          subHeading: "Nokia Solutions and Networks",
-          subSubHeading: "Oct 2020 - Jan 2021",
-          description: "Optimized warehouse operations by 40% through the development of a React.js-based inventory management system. Simplified warehouse operations with real-time data visualizations using Charts.js. Boosted team collaboration by providing feedback on code and coordinating tasks in a GitLab environment."
-        }
-        ],
-      options: {
-        backgroundImage: "linear-gradient(to top, #4481eb 0%, #04befe 100%)",
-        backgroundColor: "#4481eb",
-      },
-    },
-    interests: {
-      sectionHeader: "Interests",
-      items: [
-        {
-          mainHeading: "Algorithms and Graph Theory",
-          description:
-            "My current research interests are in combinatorics of networks and broadcasting algorithms.",
-        },
-        {
-          mainHeading: "Languages, Tools, and Frameworks",
-          description:
-            "Python, Java, Javascript, React, Node.js, Flask, git, GitHub, Visual Studio Code, Jupyter Notebooks, Figma, LaTex",
-        },
-        {
-          mainHeading: "Software Engineering",
-          description:
-            "I am an enthusiastic software engineer with experience in web development",
-          // particularly frontend development using React.
-        },
-        // {
-        //   mainHeading: "Machine Learning",
-        //   description:
-        //     "I am a passionate machine learning engineer. Problems involving Natural Language Processing (NLP) pique my interest.",
-        // },
-        {
-          mainHeading: "UI/UX Design",
-          description:
-            "I am an enthusiastic UI/UX designer. I have experience in designing user interfaces and experiences for web and mobile applications.",
-        },
-      ],
-      options: {
-        backgroundImage: grad3,
-        backgroundColor: "#667eea",
       },
     },
     certifications: {
@@ -208,16 +152,10 @@ export const content = {
       ],
       options: {
         // backgroundImage: "linear-gradient(to top, #4481eb 0%, #04befe 100%)",
-        backgroundImage: grad4,
+        backgroundImage: grad3,
         // backgroundColor: "rgb(0,150,181)",
       },
     },
   },
-  order: [
-    "experience",
-    "projects",
-    "interests",
-    "certifications",
-    "technicalSkills",
-  ],
+  order: ["experience", "projects", "certifications"],
 };
