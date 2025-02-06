41ba02bf5ce7a03bb97cf91d7dc03c3a1505f6df
akashamba <akashambashankar@gmail.com>
2025-02-05 22:48:25 -0500

    Revert "changing official img name to clear netlify cache"
    This reverts commit ea25c1df3d4044ad01c2af157ab4f9cd34428fcd.

--- src/components/intro/intro.component.jsx
+++ src/components/intro/intro.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import official from "../../assets/images/new-official.jpg";
+import official from "../../assets/images/official.jpg";
 // import getAge from '../../utilities/age-calculator.js'
 import { Grid, Container } from "@material-ui/core";
 import "./intro.styles.css";
